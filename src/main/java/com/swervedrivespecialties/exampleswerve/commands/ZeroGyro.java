0357aa61b83fa8de0e0c7a4a3e5bd763b33bab3a
CadeReinberger <32687832+CadeReinberger@users.noreply.github.com>
2019-12-07 08:10:47 -0500

    Updated ZeroGyro structure
    Added an explicit ZeroGyro command to improve readability and make structure more consistent with our team coding style

---
+++ src/main/java/com/swervedrivespecialties/exampleswerve/commands/ZeroGyro.java
@@ -0,0 +1,47 @@
+package com.swervedrivespecialties.exampleswerve.commands;
+
+import com.swervedrivespecialties.exampleswerve.subsystems.DrivetrainSubsystem;
+import org.frcteam2910.common.drivers.Gyroscope;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ZeroGyro extends Command 
+{
+  Gyroscope _gyro = DrivetrainSubsystem.getInstance().getGyroscope();
+  
+  public ZeroGyro() 
+  {
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() 
+  {
+    _gyro.setAdjustmentAngle(_gyro.getUnadjustedAngle());
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() 
+  {
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() 
+  {
+    return true;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() 
+  {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() 
+  {
+  }
+}
